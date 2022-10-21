d5f949393f5f74c3f61ac2c91cf30b9d6103708c
asrorc1k <asrornortojiev@gmail.com>
2022-10-21 18:20:00 +0300

    06

--- While06.py
+++ While06.py
@@ -7,3 +7,15 @@ def main(s):
     Returns:
         int: return answer
     """
+    i=0
+    k=0
+    s = s.lower()
+    
+    while i < len(s):
+        if s[i].isalpha():
+            if s[i]!='a' and s[i]!='u' and s[i]!='i' and s[i]!='o' and s[i]!='e':
+                k+=1
+        i+=1
+
+    return k
+print(main("CodeschoolUz"))
